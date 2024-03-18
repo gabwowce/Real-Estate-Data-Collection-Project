6484905e40a87c40c0760ee39d10b70cff38ade3
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-03-18 22:25:12 +0200

    Update sql_2.sql

--- sql_2.sql
+++ sql_2.sql
@@ -23,15 +23,23 @@ ORDER BY avrPrice DESC
 ###Analysis of apartment size and number of rooms###
 
 #The ratio of the number of rooms to the price:
-SELECT Rooms, Count(*), avg(price) avg FROM listings l 
+SELECT Rooms, Count(*) units, avg(price) AvgPrice FROM listings l 
 GROUP BY Rooms 
 WHERE l.Status = "For Sale"
 ORDER BY avg DESC 
 
-#Area and price ratio:
+#Area and price. Avarage m2 price:
 #m2 avarage price in Liyhuania:
+SELECT AVG(Price/Area) AS AvgM2Price FROM listings l 
+WHERE l.Status = "For Sale"
 
 #m2 avarage price by city/district:
-
+SELECT CityName, DistrictName, round(AVG(Price/Area),2) AS AvgM2Price FROM listings l 
+LEFT JOIN locations l2 ON l.LocationID = l2.LocationID 
+LEFT JOIN cities c ON l2.CityID = c.CityID 
+LEFT JOIN districts d ON l2.DistrictID = d.DistrictID 
+WHERE l.Status = "For Sale"
+GROUP BY CityName, DistrictName
+ORDER BY AvgM2Price DESC 
 
 ###Influence of housing condition and type of heating on the price###
